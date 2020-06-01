6b1a1841d26cdad1145c8d3e6975f156556fa65c
Joey Mckenzie <JMckenzie@spi-ind.com>
2020-06-01 11:17:15 -0700

    feat(App): Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using BlazorConduit.Services;
+
+namespace BlazorConduit
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // Add custom services
+            builder.Services.TryAddScoped<ConduitApiService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Services/ConduitApiService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorConduit.Services
+{
+    public class ConduitApiService
+    {
+        private readonly HttpClient _httpClient; 
+        private readonly string _apiBaseUrl;
+
+        public ConduitApiService(IConfiguration configuration, HttpClient httpClient) =>
+            (_httpClient, _apiBaseUrl) = (httpClient, configuration["apiBaseUrl"]);
+
+        public Task GetArticles()
+        {
+            return _httpClient.GetAsync($"{_apiBaseUrl}/articles");
+        }
+    }
+}
