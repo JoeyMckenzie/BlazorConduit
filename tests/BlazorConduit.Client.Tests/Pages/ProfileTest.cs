d94f3b25c13e13337e978c29984cb438a2cc339f
joeymckenzie <joey.mckenzie27@gmail.com>
2020-08-03 20:46:41 -0700

    feat(Tests): Add more profile tests

--- tests/BlazorConduit.Client.Tests/Pages/ProfileTest.cs
+++ tests/BlazorConduit.Client.Tests/Pages/ProfileTest.cs
@@ -1,76 +1,4 @@
 ﻿using BlazorConduit.Client.Pages;
-<<<<<<< Updated upstream
-using BlazorConduit.Client.Services.Contracts;
-using BlazorConduit.Client.Store.State;
-using BlazorConduit.Client.Tests.Mocks;
-using Bunit;
-using Fluxor;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.DependencyInjection;
-using Moq;
-using Shouldly;
-using Xunit;
-
-namespace BlazorConduit.Client.Tests.Pages
-{
-    public class ProfileTest : TestContext
-    {
-        private readonly Mock<IStateFacade> _mockFacade;
-        private readonly Mock<IState<UserState>> _mockUserState;
-        private readonly Mock<IState<ArticleState>> _mockArticleState;
-        private readonly Mock<IState<ProfileState>> _mockProfileState;
-        private readonly Mock<NavigationManager> _mockNavigationManager;
-
-        public ProfileTest()
-        {
-            _mockFacade = new Mock<IStateFacade>();
-            _mockUserState = new Mock<IState<UserState>>();
-            _mockArticleState = new Mock<IState<ArticleState>>();
-            _mockProfileState = new Mock<IState<ProfileState>>();
-            _mockNavigationManager = new Mock<NavigationManager>();
-            Services.AddSingleton(_mockFacade.Object);
-            Services.AddSingleton(_mockUserState.Object);
-            Services.AddSingleton(_mockArticleState.Object);
-            Services.AddSingleton(_mockProfileState.Object);
-            Services.AddSingleton(_mockNavigationManager.Object);
-            Services.AddMockNavigationManager();
-        }
-
-        [Fact(DisplayName = "Verify the markup is correctly rendered when page is loading")]
-        public void GivenPageIsLoading_WhenProfileStateIsAvailable_DisplaysCorrectMarkup()
-        {
-            // Arrange
-            const string expectedHtml = "<span>Loading profile...</span>";
-            var isLoadingProfileState = new ProfileState(true, null, null);
-            _mockProfileState.Setup(m => m.Value).Returns(isLoadingProfileState);
-
-            // Act
-            var component = RenderComponent<Profile>();
-
-            // Assert
-            component.MarkupMatches(expectedHtml);
-        }
-
-        [Fact(DisplayName = "Verify the markup is correctly rendered when page is loading")]
-        public void GivenPageIsLoading_WhenNoUsernameIsPassedInPath_ReroutesToHome()
-        {
-            // Arrange
-            var profileState = new ProfileState(true, null, null);
-            _mockProfileState.Setup(m => m.Value).Returns(profileState);
-
-            // Act
-            var component = RenderComponent<Profile>();
-            var navigationManager = Services.GetRequiredService<NavigationManager>() as MockNavigationManager;
-
-            // Assert
-            navigationManager.ShouldNotBeNull();
-            navigationManager!.NavigateToLocation.ShouldBe("/");
-            _mockFacade.Verify(m => m.GetUserProfile(It.IsAny<string>()), Times.Never);
-            _mockFacade.Verify(m => m.GetArticles(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()), Times.Never);
-        }
-    }
-}
-=======
 using BlazorConduit.Client.Services.Contracts;
 using BlazorConduit.Client.Store.State;
 using BlazorConduit.Client.Tests.Mocks;
@@ -136,7 +64,6 @@ namespace BlazorConduit.Client.Tests.Pages
             // Assert
             navigationManager.ShouldNotBeNull();
             navigationManager!.NavigateToLocation.ShouldBe("/");
-            _mockProfileState.VerifyAll();
             _mockFacade.Verify(m => m.GetUserProfile(It.IsAny<string>()), Times.Never);
             _mockFacade.Verify(m => m.GetArticles(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()), Times.Never);
         }
@@ -159,4 +86,3 @@ namespace BlazorConduit.Client.Tests.Pages
         }
     }
 }
->>>>>>> Stashed changes
